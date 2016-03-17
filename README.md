84a02f0b851cf2cbc2b7bae9b602414742ff49bb
kevin hu <sky790312yks@gmail.com>
2016-03-17 16:01:05 +0800

    continue to use example from react-redux

---
+++ README.md
@@ -0,0 +1,22 @@
+# React with Redux
+combine react with redux
+
+### start
+use npm & webpack
+
+```sh
+npm install
+npm run dev
+```
+
+### Thinking about react with redux
+    1. thinking about action
+    2. deal with reducer
+    3. write middleware if have
+    4. combine to store
+    5. main use Provider pass store to component/xxx.js to deal with react component
+    6. use connect to connect the store and action to react's props
+    7. write your component's view and listen event
+
+
+default dev port: 8080
---
+++ src/main.js
@@ -0,0 +1,27 @@
+import ReactDom from 'react-dom';
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from 'store/index';
+import CountButton from 'component/CountButton';
+
+// do it react way use Provider with store which's logic deal with in component/index.js
+let app = (
+    <Provider store={store}>
+        <CountButton/>
+    </Provider>
+);
+
+ReactDom.render(app, document.getElementById('app'))
+
+/*
+    Thinking about react with redux :
+
+    1. thinking about action
+    2. deal with reducer
+    3. write middleware if have
+    4. combine to store
+    5. main use Provider pass store to component/xxx.js to deal with react component
+    6. use connect to put the store and action to react's props
+    7. write your component's view and listen event
+ */
